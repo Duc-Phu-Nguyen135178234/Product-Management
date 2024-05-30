1580b89c69f5c26bdcf764cc5327f348b60ed174
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-05-30 00:41:11 -0400

    Create index.routes.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express'); // "require" the Express module
+
+const routes = require("./routes/index.routes")
+
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
 
 app.set('views','./views');
 app.set('view engine', 'pug'); 
 
-app.get('/', (req, res) => {
-    res.render("client/pages/home/index.pug");
-  });
-
-  app.get('/products', (req, res) => {
-    res.render("client/pages/products/index.pug");
-  });
+routes(app);
   
 // start the server on the port and output a confirmation to the console
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
