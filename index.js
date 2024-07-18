45cc278462fbb099923d001bcd3fec7b4b0161dd
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-07-17 23:09:50 -0400

    Bai18-1 Hien-thh-thong-bao-sau-khi-doi-trang-thai

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv'); // Declare environment
 const database = require("./config/database") // Declare connect database in config folder
 const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 
 const routes = require("./routes/client/index.route") // Declare route client
@@ -15,6 +18,13 @@ database.connect();
 const app = express(); // Obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // Assign a port
 app.use(express.static('public'));
+
+// Flash
+app.use(cookieParser('HHKALKS'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+// End Flash
+
 
 // parse application/json
 app.use(bodyParser.json());
