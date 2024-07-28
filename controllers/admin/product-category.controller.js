0c12e5ff906cca25525b47f093f43feef0903ea1
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-07-27 23:20:07 -0400

    feature edit category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -47,4 +47,47 @@ module.exports.create = async (req, res) => {
     await newCategory.save();
   
     res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+}
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+
+  const category = await ProductCategory.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  const categories = await ProductCategory.find({
+    deleted: false
+  });
+
+  const newCategories = createTreeHelper(categories);
+
+  res.render("admin/pages/products-category/edit", {
+    pageTitle: "Updating Categories",
+    categories: newCategories,
+    category: category
+  });
+}
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  if(req.body.position) {
+    req.body.position = parseInt(req.body.position);
+  } else {
+    const countCagegory = await ProductCategory.countDocuments({});
+    req.body.position = countCagegory + 1;
   }
+
+  await ProductCategory.updateOne({
+    _id: id,
+    deleted: false
+  }, req.body);
+
+  req.flash("success", "Updated is sucessful !");
+
+  res.redirect(`back`);
+}
