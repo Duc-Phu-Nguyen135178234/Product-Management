0cd4f9616c0d9d030a5a2766a9d6446b3708fd5a
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-07-30 17:54:21 -0400

    create sample page of permission

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dashboardRouter = require('./dashboard.route');
 const productsRoute = require("./product.route");
 const productsCategoryRoute = require("./product-category.route");
+const rolesRoute = require("./role.route");
 const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
@@ -12,5 +13,7 @@ module.exports = (app) => {
     app.use(`${path}/products`, productsRoute);
 
     app.use(`${path}/products-category`, productsCategoryRoute);
+
+    app.use(`${path}/roles`, rolesRoute);
 
     };
