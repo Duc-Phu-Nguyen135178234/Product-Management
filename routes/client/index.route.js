b409cb5616b0e8733c5b093608f2a335050cffba
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-12 19:42:25 -0400

    update client homw view

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,7 +2,11 @@
 const HomeRouter = require("./home.route");
 const ProductRouter = require("./product.route");
 
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
+
     app.use('/', HomeRouter);
     app.use('/products', ProductRouter);  // Use the correct variable name
     app.use("/detail/:slug", ProductRouter );
