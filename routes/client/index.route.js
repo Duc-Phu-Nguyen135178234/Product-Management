6a710637325b3325f1860d51639ce5b1f693ef04
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-12 20:10:27 -0400

    update search in header.pug

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,7 @@
 // routes/client/index.route.js
 const HomeRouter = require("./home.route");
 const ProductRouter = require("./product.route");
+const searchRoute = require("./search.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
@@ -8,7 +9,11 @@ module.exports = (app) => {
     app.use(categoryMiddleware.category);
 
     app.use('/', HomeRouter);
-    app.use('/products', ProductRouter);  // Use the correct variable name
+
+    app.use('/products', ProductRouter);  
+
     app.use("/detail/:slug", ProductRouter );
+
+    app.use("/search", searchRoute);
    
 };
