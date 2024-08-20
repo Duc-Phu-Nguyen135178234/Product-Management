30ac52d6ac65c7dbfa8cea2b3456224fa452e276
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-19 23:00:07 -0400

    add link Cart to header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,20 +1,33 @@
-//create new cookies cardId with expires Date
 const Cart = require("../../model/cart.model");
 
 module.exports.cartId = async (req, res, next) => {
-  if(!req.cookies.cartId) {
+  
+  if (!req.cookies.cartId) {
+    
     const cart = new Cart();
+    
     await cart.save();
 
+    // Set the cookie expiration time 
     const expires = 365 * 24 * 60 * 60 * 1000;
 
+    // Set a cookie named 'cartId' with the value of the new cart's ID and the expiration time
     res.cookie(
       "cartId", 
       cart.id, 
       { 
-        expires: new Date(Date.now() + expires)
+        expires: new Date(Date.now() + expires) 
       });
+  } else {
+    // If the cartId cookie exists, 
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+
+    // Set the number of products in the cart as 'cartTotal' in res.locals
+    // If the cart has no products, it defaults to 0
+    res.locals.cartTotal = cart.products.length || 0;
   }
 
-  next();
+   next();
 }
