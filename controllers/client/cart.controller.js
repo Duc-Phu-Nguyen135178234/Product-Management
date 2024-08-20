d2b411cb623cc1593d315620500e469472436825
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-19 23:16:36 -0400

    create Cart View

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,44 +1,82 @@
 
 const Cart = require("../../model/cart.model");
 
-// [POST] /cart/add/:productId
-module.exports.addPost = async (req, res) => {
+const Product = require("../../model/product.model");
+
+//[GET] /cart
+module.exports.index = async (req, res) => {
+  // Retrieve the cartId from the cookies 
   const cartId = req.cookies.cartId;
-  const productId = req.params.productId;
-  const quantity = parseInt(req.body.quantity);
 
-
+  // Fetch the cart document from the database using the cartId.
   const cart = await Cart.findOne({
     _id: cartId
   });
 
-  const existProductInCart = cart.products.find(
-    item => item.productId == productId
-  );
+  
+  cart.totalPrice = 0;
 
-  if(existProductInCart) {
-    await Cart.updateOne({
-      _id: cartId,
-      'products.productId': productId
-    }, {
-      $set: {
-        'products.$.quantity': quantity + existProductInCart.quantity
-      }
-    });
-  } else {
-    await Cart.updateOne({
-      _id: cartId
-    }, {
-        //Use $ push to add into cart if they have same object ID . If not , they will create 
-        //duplicate products in Cart
-      $push: {
-        products: {
-          productId: productId,
-          quantity: quantity
-        }
-      }
-    });
+  // Check any products in the cart.
+  if (cart.products.length > 0) {
+    
+    for (const product of cart.products) {
+      //find product in Cart
+      const productInfo = await Product.findOne({
+        _id: product.productId
+      }).select("title thumbnail slug price discountPercentage");
+
+      // Calculate the discounted price of the product.
+      productInfo.priceNew = (1 - productInfo.discountPercentage / 100) * productInfo.price;
+
+      // Attach the fetched product information to the product in the cart.
+      product.productInfo = productInfo;
+
+      // Calculate the total price for this product based on its quantity.
+      product.totalPrice = productInfo.priceNew * product.quantity;
+
+      // Add the product's total price to the cart's 
+      cart.totalPrice += product.totalPrice;
+    }
   }
 
-  res.redirect("back");
+  // Render the cart page with the cart details, passing the cart object to the view.
+  res.render("client/pages/cart/index", {
+    pageTitle: "Cart",
+    cartDetail: cart
+  });
 }
+
+// [POST] /cart/add/:productId
+module.exports.addPost = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+    const quantity = parseInt(req.body.quantity);
+    const cart = await Cart.findOne({
+      _id: cartId
+    });
+    const existProductInCart = cart.products.find(
+      item => item.productId == productId
+    );
+    if(existProductInCart) {
+      await Cart.updateOne({
+        _id: cartId,
+        'products.productId': productId
+      }, {
+        $set: {
+          'products.$.quantity': quantity + existProductInCart.quantity
+        }
+      });
+    } else {
+      await Cart.updateOne({
+        _id: cartId
+      }, {
+        $push: {
+          products: {
+            productId: productId,
+            quantity: quantity
+          }
+        }
+      });
+    }
+    res.redirect("back");
+  }
