a349e5617cc850a417094f18054533a907b51cc6
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-20 21:43:18 -0400

    Post date to server

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../model/cart.model");
 const Product = require("../../model/product.model");
+const Order = require("../../model/order.model");
 
 // [GET] /checkout/
 module.exports.index = async (req, res) => {
@@ -38,3 +39,62 @@ module.exports.index = async (req, res) => {
     cartDetail: cart // Pass the cart details to the view 
   });
 };
+
+
+//[Post] //checkout/order
+module.exports.orderPost = async (req,res) =>{
+
+    const cartId = req.cookies.cartId; // Retrieve the cart ID from the user's cookies
+    const userInfo = req.body; // Get the user information from the request body
+    
+    // create the order data
+    const orderData = {
+      userInfo: userInfo, //get from req.body
+      products: [] 
+    };
+    
+
+    // Find the cart document in the database by its ID
+    const cart = await Cart.findOne({
+      _id: cartId
+    }); 
+
+
+    // Loop through each product in the cart
+    for (const item of cart.products) { 
+
+
+    // Find productId
+      const productInfo = await Product.findOne({
+        _id: item.productId
+      }); 
+
+
+      // Add the product ID to the order data
+
+      orderData.products.push({
+        productId: item.productId, 
+
+        price: productInfo.price, 
+
+        discountPercentage: productInfo.discountPercentage, 
+
+        quantity: item.quantity 
+      }); 
+    }
+
+
+    // Create a new Order object using the order data and Save
+    const order = new Order(orderData); 
+    await order.save(); 
+    
+
+     // Clear the products in the cart by updating the cart in the database
+    await Cart.updateOne({
+      _id: cartId
+    }, {
+      products: []
+    });
+    
+    res.redirect(`/checkout/success/${order.id}`);
+}
