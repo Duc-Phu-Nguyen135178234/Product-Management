5a1bbf31f8c92e7f2409a4f46c94995ccdb2def7
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-08-23 16:13:10 -0400

    Register user(check email is exist , save data and reuturn token

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -3,6 +3,7 @@ const Account = require("../../model/account.model");
 const md5 = require('md5');
 const generateHelper = require("../../helpers/generate.helper");
 const systemConfig = require("../../config/system");
+
 // [GET] /admin/accounts
 module.exports.index = async (req, res) => {
   const records = await Account.find({
--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,7 @@
 const md5 = require("md5"); 
 
+const User = require("../../model/user.model");
+
 // Import the helper module for generating random strings
 
 const generateHelper = require("../../helpers/generate.helper"); 
@@ -13,6 +15,17 @@ module.exports.register = async (req, res) => {
 
 // [POST] /user/register
 module.exports.registerPost = async (req, res) => {
+  const existUser = await User.findOne({
+    email: req.body.email,
+    deleted: false
+  });
+
+  if(existUser) {
+    req.flash("error", "Email is existing!");
+    res.redirect("back");
+    return;
+  }
+
   const userData = {
     fullName: req.body.fullName, 
     email: req.body.email,
@@ -20,7 +33,12 @@ module.exports.registerPost = async (req, res) => {
     tokenUser: generateHelper.generateRandomString(30) // Generate a random 30-character string as the user's token
   };
 
-  console.log(userData); 
+  const user = new User(userData);
+  await user.save();
 
-  res.send("OK"); 
+  res.cookie("tokenUser", user.tokenUser);
+  req.flash("success", "Register is successful !");
+  res.redirect("/");
 };
+
+
